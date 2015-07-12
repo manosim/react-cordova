c6e98d45a59fe5bc978341366a6bdb6b06f58f5a
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-07-12 03:29:31 +0100

    React Router

--- www/src/js/app.js
+++ www/src/js/app.js
@@ -1,4 +1,32 @@
 var React = require('react');
-var Home = require('./pages/home');
+var Router = require('react-router');
 
-React.render(<Home/>, document.getElementById('app'));
+var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
+var DefaultRoute = Router.DefaultRoute;
+var RouteHandler = Router.RouteHandler;
+
+var HomePage = require('./pages/home');
+var AboutPage = require('./pages/about');
+
+var App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <RouteHandler />
+      </div>
+    );
+  }
+});
+
+var routes = (
+  <Route handler={App} path="/">
+    <DefaultRoute handler={HomePage} />
+    <Route name="home" handler={HomePage}/>
+    <Route name="about" handler={AboutPage}/>
+  </Route>
+);
+
+Router.run(routes, function (Handler) {
+  React.render(<Handler/>, document.getElementById('app'));
+});
---
+++ www/src/js/pages/about.js
@@ -0,0 +1,21 @@
+var React = require('react');
+var Router = require('react-router');
+
+var AboutPage = React.createClass({
+  mixins: [
+    Router.Navigation,
+  ],
+
+  render: function () {
+    return (
+      <div className='about-page'>
+        <div className='bar bar-header bar-positive'>
+          <button className="button button-icon ion-chevron-left" onClick={this.goBack} />
+          <h1 className='title'>About the project</h1>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = AboutPage;
